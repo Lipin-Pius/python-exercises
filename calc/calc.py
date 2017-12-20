ce4250e503db9be642fde4c0f1182b11b8cca0d7
Lipin-Pius <lipinpius005@gmail.com>
2017-12-20 22:05:14 +0530

    Commented

--- calc/calc.py
+++ calc/calc.py
@@ -6,15 +6,15 @@ import click
 @click.option('--mul', nargs=2, type=float)
 @click.option('--div', nargs=2, type=float)
 def calc(add,sub,mul,div):
-	if add and not sub and not mul and not div:
-		print (add[0]+add[1])
-	elif sub and not add and not mul and not div:
-		print (sub[0]-sub[1])
-	elif mul and not add and not sub and not div:
-		print (mul[0]*mul[1])
-	elif div and not add and not sub and not mul:
-		print (div[0]/div[1])
-if __name__=='__main__':
+	if add and not sub and not mul and not div: # if the option is only --add
+		print (add[0]+add[1]) # printing the sum of two parameters
+	elif sub and not add and not mul and not div:# if the option is only --sub
+		print (sub[0]-sub[1])# printing the subtraction of two parameters
+	elif mul and not add and not sub and not div:# if the option is only --mul
+		print (mul[0]*mul[1])# printing the product of two parameters
+	elif div and not add and not sub and not mul:# if the option is only --div
+		print (div[0]/div[1])# printing the division of two parameters
+if __name__=='__main__': # main function
 	pass
 
 
