e47ab2daea6cf6801bbb3aeab763b7edaf6f047f
Lipin-Pius <lipinpius005@gmail.com>
2018-01-02 19:31:34 +0530

    Exercise 3 Added

---
+++ Exercise 3/Exercise3.py
@@ -0,0 +1,42 @@
+#!/bin/python
+
+noofPerm = 1000001 #for millionth permutation for 0th permutation is 0123456789
+i = 1
+
+factoradic = [] # list t store the factoradic representation of noofPerm
+
+while noofPerm is not 0: 
+	factoradic.append(noofPerm%i)
+	noofPerm = noofPerm/i
+	i = i+1
+
+# Calculating the Factoradic Representaion of noofPerm
+
+charlist = [0,1,2,3,4,5,6,7,8,9] # list to be permuted
+
+output = [] # to store the millionth permutation of charlist
+
+
+if len(charlist) > len(factoradic):
+	unused = len(charlist) - len(factoradic)
+	for i in range(0,unused):
+		output.append(charlist.pop(0))
+		charlist = [ charlist[j] for j in range(0,len(charlist)) ]
+
+# to copy elements from charlist to output that need not be changed 
+
+
+while len(charlist) is not 0:
+	i = len(charlist)
+	i = factoradic[i-1]
+	output.append(charlist.pop(i))
+	charlist = [ charlist[j] for j in range(0,len(charlist)) ]
+
+# changing and copying the charlist(0th permutation) to output(millionth) based on
+# the factoradic repersentation
+
+
+str1 = ''.join(str(e) for e in output) # converting output list containing integer
+# to string to print
+
+print str1 # printing the output millionth perutation
---
+++ Exercise 3/README.md
@@ -0,0 +1,5 @@
+# Find the Millionth Permutation of 0,1,2,3,4,5,6,7,8 and 9 in lexicographic order
+
+
+Output:
+	2783915640
